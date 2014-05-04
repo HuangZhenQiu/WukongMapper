e825972baebe4d9afeab7caaeaa1edca43d4ab3b
HuangZhenQiu <huangzhenqiu0825@gmail.com>
2014-05-03 22:27:50 -0700

    create test case for collocation graph.

---
+++ test/edu/uci/eecs/wukong/energy/common/CollocationGraphTest.java
@@ -0,0 +1,36 @@
+package edu.uci.eecs.wukong.energy.common;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+import edu.uci.eecs.wukong.common.FlowBasedProcess;
+import edu.uci.eecs.wukong.common.WukongSystem;
+
+public class CollocationGraphTest extends TestCase{
+	
+	
+	public void testCollocationGraphInitization() {
+		String root = System.getProperty("user.dir");
+		try {
+			FileReader inputStream = new FileReader(new File(root + "/data/fbp.txt"));
+			FlowBasedProcess fbp = new FlowBasedProcess(FlowBasedProcess.TYPE.LINEAR);
+			fbp.initialize(new BufferedReader(inputStream));
+			
+			inputStream = new FileReader(new File(root + "/data/wukong.txt"));
+			WukongSystem system = new WukongSystem();
+			system.initialize(new BufferedReader(inputStream));
+			
+			//TODO test the collocation graph 
+		} catch (Exception e) {
+			
+		} finally {
+			
+		}
+	}
+
+}
