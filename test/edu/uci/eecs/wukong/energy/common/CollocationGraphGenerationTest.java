8995f880d20281679515b51c727ffa46ae6ef6a8
余士元 <louis29418401@gmail.com>
2014-05-20 20:37:30 +0800

    random fbp and wukongsystem generation testcase

---
+++ test/edu/uci/eecs/wukong/energy/common/CollocationGraphGenerationTest.java
@@ -0,0 +1,50 @@
+package edu.uci.eecs.wukong.energy.common;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import com.google.common.collect.ImmutableList;
+
+import junit.framework.TestCase;
+import edu.uci.eecs.wukong.common.CollocationGraph;
+import edu.uci.eecs.wukong.common.CollocationGraphNode;
+import edu.uci.eecs.wukong.common.FlowBasedProcess;
+import edu.uci.eecs.wukong.common.FlowGraph;
+import edu.uci.eecs.wukong.common.WukongSystem;
+import edu.uci.eecs.wukong.common.FlowBasedProcess.Edge;
+import edu.uci.eecs.wukong.common.FlowBasedProcess.TYPE;
+import edu.uci.eecs.wukong.util.FlowBasedProcessFactory;
+import edu.uci.eecs.wukong.util.WeightedIndependentSetSelector;
+import edu.uci.eecs.wukong.util.WuKongSystemFactory;
+
+public class CollocationGraphGenerationTest extends TestCase{
+	
+	
+	
+	public void testCollocationGraphInitization() {
+		String root = System.getProperty("user.dir");
+		try {
+			
+			FlowBasedProcessFactory fbpFactory = new FlowBasedProcessFactory(10 /* landmark number */, 20 /* class number */, 100 /* distance range */, 100 /* weight */);
+			WuKongSystemFactory wukongFactory = new WuKongSystemFactory(20 /* class number */, 10 /* device number */, 10 /* landmark number */, 100 /* distance range */);
+			
+			FlowBasedProcess fbp = fbpFactory.createFlowBasedProcess(TYPE.RANDOM);
+			WukongSystem system = wukongFactory.createRandomWuKongSystem();
+			
+			fbp.toFile(root + "/data/fbp2.txt");
+			system.toFile(root + "/data/wukong2.txt");
+			
+			
+			
+		} catch (Exception e) {
+			
+		} finally {
+			
+		}
+	}
+
+}
