664e234519c1f51568a71d011b5b888081e7cb6b
余士元 <louis29418401@gmail.com>
2014-06-20 20:33:05 +0800

    modify bug in create new co-location vertex

--- test/edu/uci/eecs/wukong/energy/common/CollocationGraphTest.java
+++ test/edu/uci/eecs/wukong/energy/common/CollocationGraphTest.java
@@ -4,17 +4,13 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.List;
-import java.util.ArrayList;
-import java.util.HashSet;
 
 import com.google.common.collect.ImmutableList;
 
 import junit.framework.TestCase;
-import edu.uci.eecs.wukong.colocation.ColocationGraph;
 import edu.uci.eecs.wukong.colocation.ColocationGraphNode;
 import edu.uci.eecs.wukong.colocation.FlowGraph;
 import edu.uci.eecs.wukong.colocation.LayeredCollocationGraph;
-import edu.uci.eecs.wukong.colocation.UpdatedColocationGraph;
 import edu.uci.eecs.wukong.common.FlowBasedProcess;
 import edu.uci.eecs.wukong.common.WukongSystem;
 import edu.uci.eecs.wukong.common.FlowBasedProcess.Edge;
@@ -41,12 +37,20 @@ public class CollocationGraphTest extends TestCase{
 				graph.addEdge(edge);
 			}
 			
-			UpdatedColocationGraph collocationGraph = new UpdatedColocationGraph(graph, system);
-			collocationGraph.print();
+//			System.out.println("Result from updated Colocation graph");
+//			UpdatedColocationGraph collocationGraph = new UpdatedColocationGraph(graph, system);
+//			collocationGraph.print();
 			
-//			LayeredCollocationGraph collocationGraph2 = new LayeredCollocationGraph(graph, system);
-//			collocationGraph2.print();
+			System.out.println("Result from layered Colocation graph");
+			LayeredCollocationGraph collocationGraph2 = new LayeredCollocationGraph(graph, system);
+			collocationGraph2.printLayeredColocationGraph();
+			WeightedIndependentSetSelector selector = new WeightedIndependentSetSelector(system, GreedyType.GWMIN);
+			List<ColocationGraphNode> nodes = selector.select_layer(graph);
 			
+			for(ColocationGraphNode node: nodes){
+				System.out.println("I am node" + node.getNodeId());
+				System.out.println(node.getInvolveWuClasses());
+			}
 //			WeightedIndependentSetSelector selector = new WeightedIndependentSetSelector(
 //					system, GreedyType.GWMIN);
 //			List<CollocationGraphNode> nodes = selector.select(graph);
